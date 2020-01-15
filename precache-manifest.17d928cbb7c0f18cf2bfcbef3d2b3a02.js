79ea173fccbdd5a27d8a0f6ae0f0ece5df6325f0
OlehTereshchuk <olehtereshchuk@ukr.net>
2020-01-15 12:42:55 +0200

    Updates

---
+++ precache-manifest.17d928cbb7c0f18cf2bfcbef3d2b3a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d148ecda2ac67f29e5fb07692775cd",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "f0db1beaea6c191fb874",
+    "url": "/react_todo-app/static/css/main.d430af95.chunk.css"
+  },
+  {
+    "revision": "90ba8cd7f2ab7ea6b562",
+    "url": "/react_todo-app/static/js/2.b9753c7f.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/react_todo-app/static/js/2.b9753c7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0db1beaea6c191fb874",
+    "url": "/react_todo-app/static/js/main.86e5b0ff.chunk.js"
+  },
+  {
+    "revision": "bb52b7febc5d3e478994",
+    "url": "/react_todo-app/static/js/runtime-main.a3745d74.js"
+  },
+  {
+    "revision": "c4a18e0528602ce0196a3ae3df193b27",
+    "url": "/react_todo-app/static/media/edit.c4a18e05.svg"
+  }
+]);
